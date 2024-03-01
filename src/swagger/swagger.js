9ac0f5a51186bac28eaa4d79418a130ef72e1b31
Tatev Hunanyan <124250439+Tatevik2000@users.noreply.github.com>
2024-03-02 01:17:00 +0400

    Update swagger.js

--- src/swagger/swagger.js
+++ src/swagger/swagger.js
@@ -1,55 +1,56 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
+const express = require('express');  
+const router = express.Router();  
+const swaggerJSDoc = require('swagger-jsdoc');  
+const swaggerUi = require('swagger-ui-express');  
+require('swagger-model-validator');  
 
-const express = require('express')
-const router = express.Router()
-
-const options = {
-  swaggerDefinition: {
-    info: {
-      title: 'AWS Demo',
-      version: '1.0.0',
-      description: 'An AWS Demo for a full stack application with Amazon ECS, applying DevOps practices',
-      contact: {
-        email: 'burkhmar@amazon.de'
-      }
-    },
-    tags: [
-      {
-        name: 'AWS Demo Endpoints',
-        description: 'Enpoints descriptions'
-      }
-    ],
-    schemes: ['http'],
-    host: '<SERVER_ALB_URL>',
-    basePath: '/'
-  },
-  apis: [
-    './src/app.js',
-  ],
-}
-
-const swaggerJSDoc = require('swagger-jsdoc')
-const swaggerUi = require('swagger-ui-express')
-const swaggerSpec = swaggerJSDoc(options)
-require('swagger-model-validator')(swaggerSpec)
-
-router.get('/json', function (req, res) {
-  res.setHeader('Content-Type', 'application/json')
-  res.send(swaggerSpec)
-})
-
-router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
-
-function validateModel(name, model) {
-  const responseValidation = swaggerSpec.validateModel(name, model, false, true)
-  if (!responseValidation.valid) {
-    console.error(responseValidation.errors)
-    throw new Error(`Model doesn't match Swagger contract`)
-  }
-}
-
-module.exports = {
-  router,
-  validateModel
-}
+const loadBalancerUrl = process.env.LOAD_BALANCER_URL;  
+  
+const options = {  
+  swaggerDefinition: {  
+    info: {  
+      title: 'AWS Demo',  
+      version: '1.0.0',  
+      description: 'An AWS Demo for a full stack application with Amazon ECS, applying DevOps practices',  
+      contact: {  
+        email: 'burkhmar@amazon.de'  
+      }  
+    },  
+    tags: [  
+      {  
+        name: 'AWS Demo Endpoints',  
+        description: 'Endpoints descriptions'  
+      }  
+    ],  
+    schemes: ['http'],  
+    // Use the load balancer URL from the environment variable  
+    host: loadBalancerUrl,  
+    basePath: '/'  
+  },  
+  apis: [  
+    './src/app.js',  
+  ],  
+};  
+  
+const swaggerSpec = swaggerJSDoc(options);  
+require('swagger-model-validator')(swaggerSpec);  
+  
+router.get('/json', function (req, res) {  
+  res.setHeader('Content-Type', 'application/json');  
+  res.send(swaggerSpec);  
+});  
+  
+router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));  
+  
+function validateModel(name, model) {  
+  const responseValidation = swaggerSpec.validateModel(name, model, false, true);  
+  if (!responseValidation.valid) {  
+    console.error(responseValidation.errors);  
+    throw new Error(`Model doesn't match Swagger contract`);  
+  }  
+}  
+  
+module.exports = {  
+  router,  
+  validateModel  
+};  
